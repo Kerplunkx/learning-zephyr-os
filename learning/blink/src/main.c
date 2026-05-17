cf455c89ffa414436ca88fc8312f399896e64a99
Guillermo Moreira <guillermomoreira01@gmail.com>
2026-05-17 16:52:28 -0500

    board overlay

--- learning/blink/src/main.c
+++ learning/blink/src/main.c
@@ -2,30 +2,23 @@
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/kernel.h>
 
-#define LED_PIN 2
-
-static const struct device *gpio_ct_dev = DEVICE_DT_GET(DT_NODELABEL(gpio0));
+static const struct gpio_dt_spec led =
+    GPIO_DT_SPEC_GET(DT_NODELABEL(blinking_led), gpios);
 
 int main() {
-  if (!device_is_ready(gpio_ct_dev)) {
+  if (!device_is_ready(led.port)) {
     return -1;
   }
 
   int ret;
-  ret = gpio_pin_configure(gpio_ct_dev, LED_PIN, GPIO_OUTPUT_ACTIVE);
+  ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
 
   if (ret != 0) {
     return -1;
   }
 
   while (true) {
-    ret = gpio_pin_set_raw(gpio_ct_dev, LED_PIN, 1);
-    if (ret != 0) {
-      return -1;
-    }
-    k_msleep(1000);
-
-    ret = gpio_pin_set_raw(gpio_ct_dev, LED_PIN, 0);
+    ret = gpio_pin_toggle_dt(&led);
     if (ret != 0) {
       return -1;
     }
